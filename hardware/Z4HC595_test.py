ed8ec5a00a1847d771d4e9d83a601ab7866cc2b0
Slink0 <medinanick5@gmail.com>
2026-03-25 22:21:43 -0500

    added multiple shift register test code

--- hardware/Z4HC595_test.py
+++ hardware/Z4HC595_test.py
@@ -2,32 +2,40 @@ import spidev
 import RPi.GPIO as GPIO
 import time
 
-LATCH = 25  # GPIO pin for RCLK (latch)
+LATCH = 25       # RCLK pin
+NUM_REGS = 8     # CHANGE THIS (e.g., 8 for full cube)
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(LATCH, GPIO.OUT)
 
-# Setup SPI
 spi = spidev.SpiDev()
-spi.open(0, 0)  # bus 0, CE0
+spi.open(0, 0)
 spi.max_speed_hz = 1000000
 
-def write_byte(value):
-    GPIO.output(LATCH, 0)   # latch LOW
-    spi.xfer([value])       # send 8 bits
-    GPIO.output(LATCH, 1)   # latch HIGH
+def write_registers(data):
+    GPIO.output(LATCH, 0)
+    spi.xfer(data)
+    GPIO.output(LATCH, 1)
 
 try:
     while True:
-        for i in range(8):
-            value = 1 << i   # shift single HIGH bit
-            write_byte(value)
-            time.sleep(0.2)
+        total_bits = NUM_REGS * 8
+
+        for i in range(total_bits):
+            data = [0] * NUM_REGS
+
+            reg_index = i // 8
+            bit_index = i % 8
+
+            data[NUM_REGS - 1 - reg_index] = 1 << bit_index
+
+            write_registers(data)
+            time.sleep(0.1)
 
 except KeyboardInterrupt:
     pass
 
 finally:
-    write_byte(0x00)  # turn everything off
+    write_registers([0] * NUM_REGS)
     spi.close()
     GPIO.cleanup()
